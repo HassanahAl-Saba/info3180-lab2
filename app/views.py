f2e0c3b0283ec1204df4a5e7ffa62c407ed040f4
HassanahAl-Saba <hassanah.al-saba@hotmail.com>
2021-02-26 15:34:42 -0500

    Final push

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
-
+import datetime
 from app import app
 from flask import render_template, request, redirect, url_for, flash
 
@@ -22,7 +22,17 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Hassanah Al-Saba")
+
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html', date_joined = format_date_joined())
+    
+
+def format_date_joined():
+    date = datetime.datetime.now()
+    return "Joined " + date.strftime("%B, %Y")
 
 
 ###
